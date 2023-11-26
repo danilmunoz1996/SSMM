99c8c513d2522e66e06f24eea83e7336a006ab9d
Danilo <danilmunoz@inf.udec.cl>
2023-11-26 13:45:50 -0300

    first commit

--- infra.sh
+++ infra.sh
@@ -4,6 +4,14 @@
 # MODELS FILE NAME
 MODELS_FILE_NAME=$1
 
+# DEPLOY?
+DEPLOY=$2
+
+# Check if infrastructure dir exists
+if [ -d "infrastructure" ]; then
+    rm -rf infrastructure
+fi
+
 # Get infra template
 cookiecutter gh:danilmunoz1996/sls-crud-infra --no-input project_name="infrastructure"
 
@@ -22,11 +30,15 @@ python generate_infra_project_file.py $MODELS_FILE_NAME
 # move project.ts file to infrastructure/lib/project.ts
 mv project.ts infrastructure/bin/project.ts
 
-# go to infrastructure
-cd infrastructure
-
-# install dependencies
-#npm install
-
 # deploy the project
-#cdk deploy --all --require-approval never
+
+if [ "$DEPLOY" = true ] ; then
+    # go to infrastructure
+    cd infrastructure
+
+    # install dependencies
+    npm install
+
+    # deploy
+    cdk deploy --all --require-approval never
+fi
--- run.sh
+++ run.sh
@@ -6,7 +6,15 @@ MODELS_CLASS_FILES_DIR=$1
 # MODELS_FILE_NAME
 MODELS_FILE_NAME=$2
 
-# Create domain directory
+# deploy?
+DEPLOY=$3
+
+# Check if domain dir exists
+if [ -d "domain" ]; then
+    rm -rf domain
+fi
+
+# Create Folder for Domain Classes
 mkdir domain
 
 # Domain Classes Definition
@@ -23,11 +31,15 @@ python backend_class_files_generator.py $MODELS_CLASS_FILES_DIR $MODELS_FILE_NAM
 # For each model in models.json execute backend.sh
 models=$(cat ${MODELS_FILE_NAME})
 
+# Check if backend dir exists
+if [ -d "backend" ]; then
+    rm -rf backend
+fi
+
 # Make backend dir
 mkdir backend
 
 for element in $(echo "${models}" | jq -r '.[]'); do
-    echo "${element}"
     ./make_backend.sh $MODELS_CLASS_FILES_DIR $element
     ./make_infra_stack.sh $MODELS_CLASS_FILES_DIR $element
 done
@@ -35,13 +47,16 @@ done
 # Delete MODELS_CLASS_FILES_DIR
 rm -rf $MODELS_CLASS_FILES_DIR
 # create and deploy infraestructure
-./infra.sh $MODELS_FILE_NAME
+./infra.sh $MODELS_FILE_NAME $DEPLOY
 
-# deploy microservices
-#for element in $(echo "${models}" | jq -r '.[]'); do
-#    echo "${element}"
-#    cd $element-service
-#    npm i
-#    sls deploy
-#    cd ..
-# done
+# if deploy is true, deploy microservices
+
+if [ "$DEPLOY" = true ] ; then
+    for element in $(echo "${models}" | jq -r '.[]'); do
+        cd backend/$element-service
+        npm i
+        sls deploy
+        cd ..
+        cd ..
+    done
+fi
